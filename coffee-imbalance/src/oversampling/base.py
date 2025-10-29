60484769bfce86d5091e948aa6b7263b64c32a07
soondow <134377303+soondow@users.noreply.github.com>
2025-10-29 20:15:04 +0900

    Delete coffee-imbalance directory

--- coffee-imbalance/src/oversampling/base.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-import pandas as pd
-
-class ISampler(ABC):
-    name: str = "base"
-    @abstractmethod
-    def fit_resample(self, X: pd.DataFrame, y: pd.Series, **params) -> tuple[pd.DataFrame, pd.Series] | tuple[None, None]:
-        raise NotImplementedError
